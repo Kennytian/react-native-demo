78604cbb9910604e90851f6527677d589c8f03e2
Kennytian <kenny@Emmas-MacBook-Pro.local>
2018-04-13 17:59:57 +0800

    v0.0.3 finish

---
+++ README.md
@@ -0,0 +1,31 @@
+# React Native Demo
+
+## Used packages
+
+- prop-types
+- react
+- react-native
+- react-navigation
+- eslint
+- pre-commit
+- jest
+
+## Change Log
+
+### v0.0.3
+
+- Add splash page
+
+### v0.0.2
+
+- Add show Modal and hide modal feature
+- Add show picture feature
+- Add navigate to other tab when modal is hidden
+
+### v0.0.1
+
+- Project can be running.
+
+## License
+
+MIT
---
+++ src/pages/LaunchPage.js
@@ -0,0 +1,95 @@
+import React, { PureComponent } from 'react';
+import { Platform, View, Text, TouchableOpacity } from 'react-native';
+import PropTypes from 'prop-types';
+
+const styles = {
+  container: {
+    flex: 1,
+  },
+  wrapper: {
+    position: 'absolute',
+    top: Platform.select({ android: 20, ios: 30 }),
+    right: 20,
+    width: 40,
+    height: 40,
+    borderRadius: 40,
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: 3,
+  },
+  text: {
+    color: '#fff',
+    fontSize: 12,
+  },
+  second: {
+    color: '#fff',
+    marginRight: 2,
+    fontSize: 11,
+  },
+  nickNameWrapper: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
+  },
+  nickName: {
+    fontFamily: 'System',
+    fontSize: 17,
+  },
+};
+
+const WAIT_COUNT = 3;
+
+export default class LaunchPage extends PureComponent {
+  static propTypes = {
+    navigation: PropTypes.object,
+  };
+
+  static defaultProps = {
+    navigation: null,
+  };
+
+  state = { cd: WAIT_COUNT };
+
+  componentDidMount() {
+    let countDown = WAIT_COUNT;
+    this.timer = setInterval(() => {
+      countDown -= 1;
+      if (countDown >= 0) {
+        this.setState({ cd: countDown });
+      } else {
+        this.goToHomePage();
+      }
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    this.clearTimer();
+  }
+
+  clearTimer() {
+    this.timer && clearInterval(this.timer);
+    this.timer = -1;
+  }
+
+  goToHomePage = () => {
+    this.clearTimer();
+    this.props.navigation.navigate('Home');
+  };
+
+  render() {
+    const secText = `${Math.ceil(this.state.cd)} s`;
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity style={styles.wrapper} onPress={this.goToHomePage}>
+          <Text allowFontScaling={false} style={styles.text}>跳过</Text>
+          <Text allowFontScaling={false} style={styles.second}>{secText}</Text>
+        </TouchableOpacity>
+
+        <View style={styles.nickNameWrapper}>
+          <Text style={styles.nickName}>Kenny</Text>
+        </View>
+      </View>
+    );
+  }
+}
